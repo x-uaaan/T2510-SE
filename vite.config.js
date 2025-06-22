827db9fd9e3e919022994dacd6b477a2bb2b44cb
x_uaaan_ <loksuan.tan@gmail.com>
2025-06-22 18:48:04 +0800

    Restore standard Laravel Vite asset hashing and clean up build command

--- vite.config.js
+++ vite.config.js
@@ -7,22 +7,6 @@ export default defineConfig({
     build: {
         outDir: 'api/public/build',
         emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/chunk-[name].js`,
-                assetFileNames: (assetInfo) => {
-                    const extType = assetInfo.name.split('.').at(1);
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        return `assets/img/[name].[ext]`;
-                    }
-                    if (/css/i.test(extType)) {
-                        return `assets/css/[name].[ext]`;
-                    }
-                    return `assets/[name].[ext]`;
-                }
-            }
-        }
     },
     plugins: [
         laravel({
