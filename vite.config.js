87e8e839baea00377bbd649ad0a8f306eddb6a67
x_uaaan_ <loksuan.tan@gmail.com>
2025-06-22 18:44:13 +0800

    Remove file hashing from Vite build to fix asset loading issues

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
     build: {
         outDir: 'api/public/build',
         emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
     },
     plugins: [
         laravel({
